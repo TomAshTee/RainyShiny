544a5361083d11433924e4d4bd9ebb4df19afabf
Tomasz Jaeschke <jaeschke.tomasz@gmail.com>
2017-03-12 21:56:26 +0100

    Alamofire -> Parese JSON file

--- RainyShiny/Constants.swift
+++ RainyShiny/Constants.swift
@@ -14,5 +14,7 @@ let LONGITUDE = "&lon="
 let APP_ID = "&appid="
 let API_KEY = "b1b15e88fa797225412429c1c50c122a1"
 
+typealias DownloadComplete = () -> ()
+
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
 
--- RainyShiny/CurrentWeather.swift
+++ RainyShiny/CurrentWeather.swift
@@ -10,18 +10,18 @@ import UIKit
 import Alamofire
 
 class CurrentWeather {
-    var _cityName: String!
-    var _date: String!
-    var _weatherType: String!
-    var _currentType: Double!
+    private var _cityName: String!
+    private var _date: String!
+    private var _weatherType: String!
+    private var _currentTemp: Double!
     
-    var cityName: String {
+    public var cityName: String {
         if _cityName == nil {
             _cityName = ""
         }
         return _cityName
     }
-    var date: String{
+    public var date: String{
         if _date == nil {
             _date = ""
         }
@@ -34,17 +34,44 @@ class CurrentWeather {
         
         return _date
     }
-    var weatherType: String {
+    public var weatherType: String {
         if _weatherType == nil{
             _weatherType = ""
         }
         return _weatherType
     }
-    var currentType: Double {
-        if _currentType == nil {
-            _currentType = 0.0
+    public var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
         }
-        return _currentType
+        return _currentTemp
+    }
+    
+    func downloadWeatherDetails(completed: @escaping DownloadComplete){
+        //Alamofire download
+        //let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)
+        Alamofire.request(CURRENT_WEATHER_URL).responseJSON { response in
+            if let dict = response.result.value as? Dictionary<String, Any> {
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                }
+                if let weather = dict["weather"] as? [Dictionary<String, Any>] {
+                    if let main = weather[0]["main"] as? String{
+                        self._weatherType = main.capitalized
+                    }
+                }
+                if let main = dict["main"] as? Dictionary<String, Any> {
+                    if let currentTemp = main["temp"] as? Double {
+                        self._currentTemp = self.KalwinToCelsius(kalwin: currentTemp)
+                    }
+                }
+            }
+        }
+        completed()
+    }
+    
+    private func KalwinToCelsius(kalwin: Double) -> (Double) {
+        return (kalwin - 273.15)
     }
     
 }
